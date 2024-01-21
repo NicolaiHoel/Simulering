fc73dda21c445d923c37dc19c107f595480f33a0
Nicolai Hoel <nicolai.hoel@banenor.no>
2024-01-21 20:27:44 +0100

    Removed some comments

--- Simulering/Program.cs
+++ Simulering/Program.cs
@@ -12,15 +12,7 @@ internal class Program
 
 
         DataAccsess.WriteWaterPotenialToFile(waterPotenialList);
-        //Write waterPotenialList to CSV
-        /*foreach(var water in waterPotenialList)
-        {
-            if (water.IceAmount < 0) Console.WriteLine("ice {0}", water.IceAmount);
-            if (water.SnowAmount < 0) Console.WriteLine("snow {0}", water.SnowAmount);
-            if (water.WaterRunoff < 0) Console.WriteLine("run {0}", water.WaterRunoff);
-            if (water.WaterAmount < 0) Console.WriteLine("water {0}", water.WaterAmount);
-            if (water.WaterAmount/ water.SnowAmount > 0.6) Console.WriteLine("Too much water {0}", water.WaterAmount);
-        }*/
+        
         Console.WriteLine("Simulation has ended");
     }
 }
